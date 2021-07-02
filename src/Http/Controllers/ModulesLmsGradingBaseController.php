c6cd1febbe22c0edb215b9b88696b3195219ae58
MusahMusah <shehialiyu0@gmail.com>
2021-07-02 11:00:43 +0100

    Updates added

--- src/Http/Controllers/ModulesLmsGradingBaseController.php
+++ src/Http/Controllers/ModulesLmsGradingBaseController.php
@@ -3,14 +3,65 @@
 namespace Modullo\ModulesLmsGradingBase\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Exception\GuzzleException;
+use Hostville\Modullo\Sdk;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Request;
 
 class ModulesLmsGradingBaseController extends Controller
 {
-    public function allNotes()
+    protected Sdk $sdk;
+    public function __construct(Sdk $sdk)
     {
-        return view('modules-lms-grading-base::learners.notes.index');
+        $this->sdk = $sdk;
     }
 
+    //    Modules
+    protected function getModules(){
+        $query = $this->sdk->createModuleService();
+        $query = $query->addQueryArgument('limit',100);
+        $path = [''];
+        return $query->send('get', $path);
+    }
+    
+    // Notes
+    protected function getNotes(){
+        $query = $this->sdk->createLearnerNoteService();
+        $query = $query->addQueryArgument('limit',100);
+        $path = ['all'];
+        return $query->send('get', $path);
+    }
+
+    public function allNotes(Sdk $sdk)
+    {
+
+        if ($this->getNotes()->isSuccessful()){
+            $response = $this->getNotes()->getData();
+            $data = $response['notes'];
+            return view('modules-lms-grading-base::learners.notes.index',compact('data'));
+        }
+        $data = ['error' => 'unable to fetch the requested resource'];
+        return view('modules-lms-grading-base::learners.notes.index',compact('data'));
+    }
+
+    public function storeNote(string $module_id, Request $request, Sdk $sdk) 
+    {
+        $resource = $sdk->createLearnerNoteService();
+        $path = ['save', $module_id];
+        $resource = $resource
+        ->addBodyParam('note',$request->note);
+        $response = $resource->send('post',$path);
+        if (!$response->isSuccessful()) {
+            $response = $response->getData();
+            if ($response['errors'][0]['code'] === '005') return response()->json(['error' => $response['errors'][0]['source'] ?? ''],$response['errors'][0]['status']);
+            return response()->json(['error' => $response['errors'][0]['title'] ?? ''],$response['errors'][0]['status']);
+
+        }
+        return response()->json(['message' => 'Note Successfully Saved!'], 200);
+    }
+
+    // Grades
     public function allGrades()
     {
         return view('modules-lms-grading-base::learners.grades.index');
--- src/resources/views/learners/notes/index.blade.php
+++ src/resources/views/learners/notes/index.blade.php
@@ -7,9 +7,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('vendor/GradingBaseAssets/learners-notes/assets/css/styles.css') }}">
     <style>
-        .breadcrumb-item + .breadcrumb-item::before {
+        .breadcrumb-item+.breadcrumb-item::before {
             content: ">>";
         }
+
     </style>
 @endsection
 @section('head_js')
@@ -27,16 +28,15 @@
 
     @include('modules-lms-base::navigation',['type' => 'learner'])
     <div id="app">
-        <breadcrumbs 
-            :items="[
-                {url: '/tenant/dashboard', title: 'Home', active: false},
-                {url: '', title: 'All Notes', active: true},
-            ]">
+        <breadcrumbs :items="[
+                    {url: '/tenant/dashboard', title: 'Home', active: false},
+                    {url: '', title: 'All Notes', active: true},
+                ]">
         </breadcrumbs>
         <b-container>
             <b-row>
                 <b-col>
-                    <learners-notes></learners-notes>
+                    <learners-notes :note-data="notes"></learners-notes>
                 </b-col>
             </b-row>
         </b-container>
@@ -48,15 +48,46 @@
 
     <script src="{{ asset('vendor/GradingBaseAssets/learners-notes/components/LearnersNotes.js') }}"></script>
     <script src="{{ asset('vendor/breadcrumbs/BreadCrumbs.js') }}"></script>
-    <script src="{{ asset('vendor/GradingBaseAssets/learners-notes/app.js') }}"></script>
+    {{-- <script src="{{ asset('vendor/GradingBaseAssets/learners-notes/app.js') }}"></script> --}}
+    <link href="https://unpkg.com/@morioh/v-quill-editor/dist/editor.css" rel="stylesheet">
+    <script src="https://unpkg.com/@morioh/v-quill-editor/dist/editor.min.js" type="text/javascript"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue-loading-overlay@3"></script>
+    <link href="https://cdn.jsdelivr.net/npm/vue-loading-overlay@3/dist/vue-loading.css" rel="stylesheet">
+    <!-- Init the plugin and component-->
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script>
-        ClassicEditor
-            .create( document.querySelector( '#editor' ) )
-            .catch( error => {
-                console.error( error );
-            } );
+        Vue.use(VueLoading);
+        Vue.component('loading', VueLoading)
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+            }
+    </script>
+    <script>
+        var app = new Vue({
+            el: '#app',
+            data: {
+                name: 'Musah Musah!',
+                notes: {!! json_encode($data) !!}
+            },
+            methods: {
+
+            }
+        })
     </script>
 
 @endsection
-
-
